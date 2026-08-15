3789a0c60373a8bbea86547f7d30e97dd2276709
rovicperez154-a11y <rovicperez154@gmail.com>
2026-08-15 19:55:10 +0800

    Add files via upload

---
+++ code_challenge.py
@@ -0,0 +1,22 @@
+#jahshdmsnhj	
+
+
+
+name= '\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t'
+sit= '\t\n\t\n\t\t\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t'
+chair= '\t\n\n\t\t\t\t\t\t\t*\t\t\t\t*\t\t\t\t\t'
+bol= '\t\n\n\t\t\t\t\t\t*\t\t\t\t\t\t*\t\t\t\t'
+fit= '\t\n\n\t\t\t\t\t*\t\t\t Hi, my  name  is\t\t\t*\t'
+dur= '\t\n\n\t\t\t\t\t\t*\t\t     "rovic"\t\t\t*\t\t\t\t\t'
+ter= '\t\n\n\t\t\t\t\t\t\t*\t\t\t\t*\t\t\t\t\t'
+ert= '\t\n\t\n\t\t\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t'
+tuu= '\t\n\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t'
+
+
+
+
+
+
+
+
+print(name,sit,chair,bol,fit,dur,ter,ert,tuu,)
