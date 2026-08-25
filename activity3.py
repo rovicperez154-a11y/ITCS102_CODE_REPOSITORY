19c95662a34232016fd26d11eb430952413bac92
rovicperez154-a11y <rovicperez154@gmail.com>
2026-08-25 12:16:45 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,23 @@
+
+#interger variable
+schoolRooms =40
+Professor = 45
+Building = 4
+
+#float variable
+myAllowance = 150.50
+jacketPrice = 599.99
+pi = 3.1415
+
+#string variable
+mySurename = "perez"
+myFavoriteSong ="kathang isip"
+email= "rovicperez121@gmail.com"
+
+
+#boolean variable 
+isStudent = True
+isBoy = True
+haveSister = False
+
+print("my email is",email,)
