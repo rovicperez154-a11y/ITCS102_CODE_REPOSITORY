8854eaf4fa02e8b13baef56f676119f6fd2cc128
rovicperez154-a11y <rovicperez154@gmail.com>
2026-08-25 12:17:22 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,13 @@
+#escape sequence
+#is used to modify inline strings
+#\n
+#\t
+#\r
+#\\
+#\*
+#\b
+
+motto1 = "Kaya ng iba,edi ipagawa mo sa kanila"
+motto2 = "walang mahirap sa nag sisikap"
+
+print("walang mahirap sa nag sisikap.\nKung kaya ng iba,edi ipaga mo sa kanila")
