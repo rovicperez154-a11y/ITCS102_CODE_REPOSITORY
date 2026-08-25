8c44a151ef47d50c96f6641540cbd99dea142e6f
rovicperez154-a11y <rovicperez154@gmail.com>
2026-08-25 12:02:36 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,19 @@
+a= 56
+b= 23
+sum= a + b 
+sub= a - b 
+product= a*b 
+div= a / b
+exp= 29**56
+mod= 23%10
+
+print("The sum of",a,"plus",b,"is", sum)
+print("The sum of",a,"minus",b,"is", sub)
+print("The sum of",a,"times",b,"is", product)
+print("The sum of",a,"divided by",b,"is", div)
+print("The sum of",a,"rounded by",b,"is", a // b)
+print("The sum of",a,"exponent",b,"is", exp)
+print("The sum of",a,"modulus",b,"is", mod)
+
+
+
