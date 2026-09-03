e920ae64b286920396486bc97e53278cb13bf62b
rovicperez154-a11y <rovicperez154@gmail.com>
2026-09-03 12:31:00 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,16 @@
+likes= "" 
+
+a = input("what do you like ? -->")
+likes+= a + ", "
+
+b = input("what else ? -->")
+likes+= b + ", "
+
+c = input("what else ? -->")
+likes+= c + ", "
+
+d = input("what else ? -->")
+likes+= d + ", "
+
+
+print("So, I like ", likes)
