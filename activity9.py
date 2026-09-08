6453039aedac2d2e17d560327c51e5ee993acfa5
rovicperez154-a11y <rovicperez154@gmail.com>
2026-09-08 12:40:26 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,26 @@
+x = 23
+y =15
+
+#1 
+print( x > y )
+
+#2
+print( x < y )
+
+#3
+print( x >= y )
+
+#4
+print( x <= y )
+
+#5
+print( x == y )
+
+#6
+print( x != y )
+
+text1 = 'example'
+text2 = 'Example'
+
+#7
+print( text1 == text2 )
